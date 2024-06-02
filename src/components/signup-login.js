324a0b2cdc3c5904aa74bb2f7f6ee5613eb45e6b
LogarajP <logupalani2003@gmail.com>
2024-06-02 21:07:58 +0530

    start project

---
+++ src/components/signup-login.js
@@ -0,0 +1,11 @@
+import "../App.css";
+export default function SignUpLogin() {
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <div className="signup"></div>
+        <div style={{ height: 300, width: 400, backgroundColor: "red" }}></div>
+      </div>
+    </div>
+  );
+}
